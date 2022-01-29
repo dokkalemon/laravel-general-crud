d2154d667155d884752f786f9ae0e30d0ad3551b
dokkalemon <dottori.roberto@gmail.com>
2022-01-29 18:33:14 +0100

    create header and footer, mix

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Basket Players Database</title>
+
+    {{-- Bootstrap --}}
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap.min.css' integrity='sha512-GQGU0fMMi238uA+a/bdWJfpUGKUkBdgfFdgBm72SUQ6BeyWjoY/ton0tEjH+OSH9iP4Dfh+7HM0I9f5eR0L/4w==' crossorigin='anonymous'/>
+
+    {{-- Fontawesome --}}
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css' integrity='sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==' crossorigin='anonymous'/>
+
+    {{-- General Style --}}
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+</head>
+<body>
+    {{-- Header --}}
+    @include('partials.header')
+
+    {{-- Main Content --}}
+    <main class="container-fluid">
+        @yield('content')
+    </main>
+
+    {{-- Footer --}}
+    @include('partials.footer')
+</body>
+</html>
