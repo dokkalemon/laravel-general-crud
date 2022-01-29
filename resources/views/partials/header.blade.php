d22ff483386fd1ca2d09107ef4a42ce61358b887
dokkalemon <dottori.roberto@gmail.com>
2022-01-29 18:43:03 +0100

    set general style

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-          <a class="navbar-brand" href="#">Basket Players DB</a>
+          <a class="navbar-brand text-white" href="#">Basket Players DB</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -10,8 +10,6 @@
               <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="#">Home</a>
               </li>
-              
-            
           </div>
         </div>
       </nav>
