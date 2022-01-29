9581b2f2a682d7617584270ced62bab3927c02b9
dokkalemon <dottori.roberto@gmail.com>
2022-01-29 18:59:54 +0100

    modify header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -10,6 +10,12 @@
               <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="#">Home</a>
               </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Database</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Inserisci un Giocatore</a>
+              </li>
           </div>
         </div>
       </nav>
